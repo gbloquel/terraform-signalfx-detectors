e6faaa8de4f90ad5dc7a14e689e93ee69d4fb5ee
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-28 10:21:35 -0700

    Update outputs.tf

--- cloud/gcp/cloud-sql/common/outputs.tf
+++ cloud/gcp/cloud-sql/common/outputs.tf
@@ -1,6 +1,6 @@
-output "cpu_utilization_id" {
-  description = "id for detector cpu_utilization"
-  value       = signalfx_detector.cpu_utilization.*.id
+output "heartbeat_id" {
+  description = "id for detector heartbeat"
+  value       = signalfx_detector.heartbeat.*.id
 }
 
 output "disk_utilization_id" {
@@ -8,9 +8,19 @@ output "disk_utilization_id" {
   value       = signalfx_detector.disk_utilization.*.id
 }
 
+output "disk_utilization_forecast_id" {
+  description = "id for detector disk_utilization_forecast"
+  value       = signalfx_detector.disk_utilization_forecast.*.id
+}
+
 output "memory_utilization_id" {
   description = "id for detector memory_utilization"
   value       = signalfx_detector.memory_utilization.*.id
+}
+
+output "memory_utilization_forecast_id" {
+  description = "id for detector memory_utilization_forecast"
+  value       = signalfx_detector.memory_utilization_forecast.*.id
 }
 
 output "failover_unavailable_id" {
