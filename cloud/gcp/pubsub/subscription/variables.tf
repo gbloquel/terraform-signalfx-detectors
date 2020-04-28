c6ecd0d79bdb2d1fedd72064f2443729c432da1c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-28 12:24:53 -0700

    Update variables.tf

--- cloud/gcp/pubsub/subscription/variables.tf
+++ cloud/gcp/pubsub/subscription/variables.tf
@@ -97,7 +97,7 @@ variable "oldest_unacked_message_notifications_critical" {
 variable "oldest_unacked_message_aggregation_function" {
   description = "Aggregation function and group by for oldest_unacked_message detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ""
+  default     = ".mean(by=['subscription_id'])"
 }
 
 variable "oldest_unacked_message_transformation_function" {
@@ -165,7 +165,7 @@ variable "push_latency_notifications_critical" {
 variable "push_latency_aggregation_function" {
   description = "Aggregation function and group by for push_latency detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ""
+  default     = ".mean(by=['subscription_id'])"
 }
 
 variable "push_latency_transformation_function" {
@@ -190,4 +190,90 @@ variable "push_latency_threshold_warning" {
   description = "Warning threshold for push_latency detector"
   type        = number
   default     = 1000
+}
+
+# Push_latency_anomaly detectors
+
+variable "push_latency_anomaly_disabled" {
+  description = "Disable all alerting rules for push_latency_anomaly detector"
+  type        = bool
+  default     = null
+}
+
+variable "push_latency_anomaly_disabled_critical" {
+  description = "Disable critical alerting rule for push_latency_anomaly detector"
+  type        = bool
+  default     = null
+}
+
+variable "push_latency_anomaly_notifications" {
+  description = "Notification recipients list for every alerting rules of push_latency_anomaly detector"
+  type        = list
+  default     = []
+}
+
+variable "push_latency_anomaly_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of push_latency_anomaly detector"
+  type        = list
+  default     = []
+}
+
+variable "push_latency_anomaly_aggregation_function" {
+  description = "Aggregation function and group by for push_latency_anomaly detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['subscription_id'])"
+}
+
+variable "push_latency_anomaly_transformation_function" {
+  description = "Transformation function for push_latency_anomaly detector (mean, min, max)"
+  type        = string
+  default     = "mean"
+}
+
+variable "push_latency_anomaly_transformation_window" {
+  description = "Transformation window for push_latency_anomaly detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "10m"
+}
+
+variable "push_latency_anomaly_threshold_critical" {
+  description = "Critical threshold for push_latency_anomaly detector"
+  type        = number
+  default     = 2
+}
+
+variable "push_latency_anomaly_current_window" {
+  description = "Window being tested for anomalous values"
+  type        = string
+  default     = "5m"
+}
+
+variable "push_latency_anomaly_historical_window" {
+  description = "Window that defines normal values"
+  type        = string
+  default     = "1h"
+}
+
+variable "push_latency_anomaly_fire_num_stddev" {
+  description = "Number of standard deviations different from historical mean required to trigger, should be >= 0"
+  type        = number
+  default     = 3
+}
+
+variable "push_latency_anomaly_clear_num_stddev" {
+  description = "Number of standard deviations different from historical mean required to clear, should be >= 0"
+  type        = number
+  default     = 2.5
+}
+
+variable "push_latency_anomaly_orientation" {
+  description = "Specifies whether detect fires when signal is above, below, or out-of-band (options: above, below, out_of_band)"
+  type        = string
+  default     = "above"
+}
+
+variable "push_latency_anomaly_calculation_mode" {
+  description = "Specifies whether to use exponentially weighted or usual moving average (options: vanilla, ewma)"
+  type        = string
+  default     = "vanilla"
 }
