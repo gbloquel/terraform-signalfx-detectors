1ad6584590cbb1b55188a36b7b262e4d299f35ee
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-04-27 17:09:41 -0700

    Update detectors-lb.tf
    removed heartbeat detector as there isn't a metric always reported from load balancer even if no traffic

--- cloud/gcp/lb/detectors-lb.tf
+++ cloud/gcp/lb/detectors-lb.tf
@@ -1,22 +1,3 @@
-resource "signalfx_detector" "heartbeat" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Load Balancer heartbeat"
-
-	program_text = <<-EOF
-		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('https/total_latencies' and ${module.filter-tags.filter_custom}).publish('signal')
-		not_reporting.detector(stream=signal, resource_identifier=['url_map_name'], duration='${var.heartbeat_timeframe}').publish('CRIT')
-	EOF
-
-	rule {
-		description           = "has not reported in ${var.heartbeat_timeframe}"
-		severity              = "Critical"
-		detect_label          = "CRIT"
-		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
-		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} on {{{dimensions}}}"
-	}
-}
-
 resource "signalfx_detector" "error_rate_4xx" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Load Balancer 4xx error rate"
 
