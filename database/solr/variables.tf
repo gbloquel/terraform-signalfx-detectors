fb9e2a9c00575c1e2c6909da1e550ef5a14f20cc
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-04-29 21:14:23 -0700

    Update variables.tf

--- database/solr/variables.tf
+++ database/solr/variables.tf
@@ -97,7 +97,7 @@ variable "searcher_warmup_time_notifications_critical" {
 variable "searcher_warmup_time_aggregation_function" {
   description = "Aggregation function and group by for searcher_warmup_time detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ".min(by=['Instance'])"
+  default     = ".min(by=['instance'])"
 }
 
 variable "searcher_warmup_time_transformation_function" {
