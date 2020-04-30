90b866c69f1848285e48c890ef49642949273667
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-30 15:52:42 -0700

    Update outputs.tf

--- database/mysql/outputs.tf
+++ database/mysql/outputs.tf
@@ -7,3 +7,13 @@ output "mysql_slow_id" {
   description = "id for detector mysql_slow"
   value       = signalfx_detector.mysql_slow.*.id
 }
+
+output "mysql_threads_anomaly_id" {
+  description = "id for detector mysql_threads_anomaly"
+  value       = signalfx_detector.mysql_threads_anomaly.*.id
+}
+
+output "mysql_questions_anomaly_id" {
+  description = "id for detector mysql_questions_anomaly"
+  value       = signalfx_detector.mysql_questions_anomaly.*.id
+}
