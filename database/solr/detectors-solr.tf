c1db507fb38cfecb932057d800e06d16490bfb38
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-04-29 21:13:17 -0700

    Update detectors-solr.tf

--- database/solr/detectors-solr.tf
+++ database/solr/detectors-solr.tf
@@ -1,10 +1,10 @@
 resource "signalfx_detector" "heartbeat" {
- 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Solr heartbeat"
+ 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Apache Solr heartbeat"
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('counter.solr.http_requests' and ${module.filter-tags.filter_custom})
-		not_reporting.detector(stream=signal, resource_identifier=['Instance'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+		not_reporting.detector(stream=signal, resource_identifier=['instance'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
 	rule {
@@ -18,7 +18,7 @@ resource "signalfx_detector" "heartbeat" {
 }
 
 resource "signalfx_detector" "searcher_warmup_time" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Solr searcher warmup time"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Apache Solr searcher warmup time"
 
 	program_text = <<-EOF
 		signal = data('gauge.solr.searcher_warmup' and ${module.filter-tags.filter_custom})${var.searcher_warmup_time_aggregation_function}.${var.searcher_warmup_time_transformation_function}(over='${var.searcher_warmup_time_transformation_window}').publish('signal')
