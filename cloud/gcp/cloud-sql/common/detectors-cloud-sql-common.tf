146983ccbb93157c57b8275bbf3067de6c51365b
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-13 17:22:36 -0700

    Update detectors-cloud-sql-common.tf

--- cloud/gcp/cloud-sql/common/detectors-cloud-sql-common.tf
+++ cloud/gcp/cloud-sql/common/detectors-cloud-sql-common.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('database/cpu/usage_time' and ${module.filter-tags.filter_custom}).publish('signal')
+		signal = data('database/cpu/usage_time', ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['database_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,7 +21,7 @@ resource "signalfx_detector" "cpu_utilization" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Cloud SQL CPU utilization"
 
 	program_text = <<-EOF
-		A = data('database/cpu/utilization' and ${module.filter-tags.filter_custom})${var.cpu_utilization_aggregation_function}
+		A = data('database/cpu/utilization', ${module.filter-tags.filter_custom})${var.cpu_utilization_aggregation_function}
 		signal = (A*100).${var.cpu_utilization_transformation_function}(over='${var.cpu_utilization_transformation_window}').publish('signal')
 		detect(when(signal > ${var.cpu_utilization_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.cpu_utilization_threshold_warning}) and when(signal <= ${var.cpu_utilization_threshold_critical})).publish('WARN')
@@ -50,7 +50,7 @@ resource "signalfx_detector" "disk_utilization" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Cloud SQL disk utilization"
 
 	program_text = <<-EOF
-		A = data('database/disk/utilization' and ${module.filter-tags.filter_custom})${var.disk_utilization_aggregation_function}
+		A = data('database/disk/utilization', ${module.filter-tags.filter_custom})${var.disk_utilization_aggregation_function}
 		signal = (A*100).${var.disk_utilization_transformation_function}(over='${var.disk_utilization_transformation_window}').publish('signal')
 		detect(when(signal > ${var.disk_utilization_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.disk_utilization_threshold_warning}) and when(signal <= ${var.disk_utilization_threshold_critical})).publish('WARN')
@@ -98,7 +98,7 @@ resource "signalfx_detector" "memory_utilization" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Cloud SQL memory utilization"
 
 	program_text = <<-EOF
-		A = data('database/memory/utilization' and ${module.filter-tags.filter_custom})${var.memory_utilization_aggregation_function}
+		A = data('database/memory/utilization', ${module.filter-tags.filter_custom})${var.memory_utilization_aggregation_function}
 		signal = (A*100).${var.memory_utilization_transformation_function}(over='${var.memory_utilization_transformation_window}').publish('signal')
 		detect(when(signal > ${var.memory_utilization_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.memory_utilization_threshold_warning}) and when(signal <= ${var.memory_utilization_threshold_critical})).publish('WARN')
@@ -146,7 +146,7 @@ resource "signalfx_detector" "failover_unavailable" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Cloud SQL failover unavailable"
 
 	program_text = <<-EOF
-		signal = data('database/available_for_failover' and ${module.filter-tags.filter_custom})${var.failover_unavailable_aggregation_function}.${var.failover_unavailable_transformation_function}(over='${var.failover_unavailable_transformation_window}').publish('signal')
+		signal = data('database/available_for_failover', ${module.filter-tags.filter_custom})${var.failover_unavailable_aggregation_function}.${var.failover_unavailable_transformation_function}(over='${var.failover_unavailable_transformation_window}').publish('signal')
 		detect(when(signal <= ${var.failover_unavailable_threshold_critical})).publish('CRIT')
 		detect(when(signal <= ${var.failover_unavailable_threshold_warning}) and when(signal > ${var.failover_unavailable_threshold_critical})).publish('WARN')
 	EOF
