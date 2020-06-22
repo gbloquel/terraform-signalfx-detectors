b1ef50b7df826b97380d942cc63f4f8fe402a54b
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-22 15:22:35 -0700

    Update detectors-postgresql.tf

--- database/postgresql/detectors-postgresql.tf
+++ database/postgresql/detectors-postgresql.tf
@@ -18,32 +18,32 @@ resource "signalfx_detector" "heartbeat" {
 }
 
 resource "signalfx_detector" "too_many_locks" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] PostgreSQL number of locks"
+  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] PostgreSQL number of locks"
 
-	program_text = <<-EOF
+  program_text = <<-EOF
 		from signalfx.detectors.aperiodic import conditions
 		signal = data('postgres_deadlocks', filter=filter('plugin', 'postgresql') and ${module.filter-tags.filter_custom})${var.too_many_locks_aggregation_function}.${var.too_many_locks_transformation_function}(over='${var.too_many_locks_transformation_window}').publish('signal')
 		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.too_many_locks_threshold_critical}, ${var.too_many_locks_threshold_critical}, 'above', lasting('${var.too_many_locks_aperiodic_duration}', ${var.too_many_locks_aperiodic_percentage}), 'observed')
 		ON_Condition_WARN = conditions.generic_condition(signal, ${var.too_many_locks_threshold_warning}, ${var.too_many_locks_threshold_critical}, 'within_range', lasting('${var.too_many_locks_aperiodic_duration}', ${var.too_many_locks_aperiodic_percentage}), 'observed', strict_2=False)
 		detect(ON_Condition_CRIT, off=when(signal is None, '${var.too_many_locks_clear_duration}')).publish('CRIT')
 		detect(ON_Condition_WARN, off=when(signal is None, '${var.too_many_locks_clear_duration}')).publish('WARN')
-	EOF
+  EOF
 
-	rule {
-		description           = "are too high > ${var.too_many_locks_threshold_critical}"
-		severity              = "Critical"
-		detect_label          = "CRIT"
-		disabled              = coalesce(var.too_many_locks_disabled_critical, var.too_many_locks_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.too_many_locks_notifications_critical, var.too_many_locks_notifications, var.notifications)
-		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
-	}
+  rule {
+    description           = "are too high > ${var.too_many_locks_threshold_critical}"
+    severity              = "Critical"
+    detect_label          = "CRIT"
+    disabled              = coalesce(var.too_many_locks_disabled_critical, var.too_many_locks_disabled, var.detectors_disabled)
+    notifications         = coalescelist(var.too_many_locks_notifications_critical, var.too_many_locks_notifications, var.notifications)
+    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+  }
 
-	rule {
-		description           = "are too high > ${var.too_many_locks_threshold_warning}"
-		severity              = "Warning"
-		detect_label          = "WARN"
-		disabled              = coalesce(var.too_many_locks_disabled_warning, var.too_many_locks_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.too_many_locks_notifications_warning, var.too_many_locks_notifications, var.notifications)
-		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
-	}
+  rule {
+    description           = "are too high > ${var.too_many_locks_threshold_warning}"
+    severity              = "Warning"
+    detect_label          = "WARN"
+    disabled              = coalesce(var.too_many_locks_disabled_warning, var.too_many_locks_disabled, var.detectors_disabled)
+    notifications         = coalescelist(var.too_many_locks_notifications_warning, var.too_many_locks_notifications, var.notifications)
+    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+  }
 }
