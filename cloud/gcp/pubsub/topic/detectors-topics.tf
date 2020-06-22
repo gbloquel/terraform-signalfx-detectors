b4b488141a17c718d9a08365dc7579907532610a
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-22 16:00:42 -0700

    Update detectors-topics.tf

--- cloud/gcp/pubsub/topic/detectors-topics.tf
+++ cloud/gcp/pubsub/topic/detectors-topics.tf
@@ -2,11 +2,13 @@ resource "signalfx_detector" "sending_operations" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Pub/Sub Topic sending messages operations"
 
   program_text = <<-EOF
-		from signalfx.detectors.aperiodic import aperiodic
+		from signalfx.detectors.aperiodic import conditions
 		signal = data('topic/send_message_operation_count', filter=filter('monitored_resource', 'pubsub_topic') and ${module.filter-tags.filter_custom})${var.sending_operations_aggregation_function}.${var.sending_operations_transformation_function}(over='${var.sending_operations_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.sending_operations_threshold_critical}, 'below', lasting('${var.sending_operations_aperiodic_duration}', ${var.sending_operations_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.sending_operations_threshold_warning}, ${var.sending_operations_threshold_critical}, 'within_range', lasting('${var.sending_operations_aperiodic_duration}', ${var.sending_operations_aperiodic_percentage}), upper_strict=False).publish('WARN')
-	EOF
+		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.sending_operations_threshold_critical}, ${var.sending_operations_threshold_critical}, 'below', lasting('${var.sending_operations_aperiodic_duration}', ${var.sending_operations_aperiodic_percentage}), 'observed')
+		ON_Condition_WARN = conditions.generic_condition(signal, ${var.sending_operations_threshold_warning}, ${var.sending_operations_threshold_critical}, 'within_range', lasting('${var.sending_operations_aperiodic_duration}', ${var.sending_operations_aperiodic_percentage}), 'observed', strict_2=False)
+		detect(ON_Condition_CRIT, off=when(signal is None, '${var.sending_operations_clear_duration}')).publish('CRIT')
+		detect(ON_Condition_WARN, off=when(signal is None, '${var.sending_operations_clear_duration}')).publish('WARN')
+  EOF
 
   rule {
     description           = "are too low <= ${var.sending_operations_threshold_critical}"
@@ -25,18 +27,19 @@ resource "signalfx_detector" "sending_operations" {
     notifications         = coalescelist(var.sending_operations_notifications_warning, var.sending_operations_notifications, var.notifications)
     parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
   }
-
 }
 
 resource "signalfx_detector" "unavailable_sending_operations" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Pub/Sub Topic sending unavailable messages"
 
   program_text = <<-EOF
-		from signalfx.detectors.aperiodic import aperiodic
+		from signalfx.detectors.aperiodic import conditions
 		signal = data('topic/send_message_operation_count', filter=filter('monitored_resource', 'pubsub_topic') and filter('response_code', 'unavailable') and ${module.filter-tags.filter_custom})${var.unavailable_sending_operations_aggregation_function}.${var.unavailable_sending_operations_transformation_function}(over='${var.unavailable_sending_operations_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.unavailable_sending_operations_threshold_critical}, 'above', lasting('${var.unavailable_sending_operations_aperiodic_duration}', ${var.unavailable_sending_operations_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.unavailable_sending_operations_threshold_warning}, ${var.unavailable_sending_operations_threshold_critical}, 'within_range', lasting('${var.unavailable_sending_operations_aperiodic_duration}', ${var.unavailable_sending_operations_aperiodic_percentage}), upper_strict=False).publish('WARN')
-	EOF
+		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.unavailable_sending_operations_threshold_critical}, ${var.unavailable_sending_operations_threshold_critical}, 'above', lasting('${var.unavailable_sending_operations_aperiodic_duration}', ${var.unavailable_sending_operations_aperiodic_percentage}), 'observed')
+		ON_Condition_WARN = conditions.generic_condition(signal, ${var.unavailable_sending_operations_threshold_warning}, ${var.unavailable_sending_operations_threshold_critical}, 'within_range', lasting('${var.unavailable_sending_operations_aperiodic_duration}', ${var.unavailable_sending_operations_aperiodic_percentage}), 'observed', strict_2=False)
+		detect(ON_Condition_CRIT, off=when(signal is None, '${var.unavailable_sending_operations_clear_duration}')).publish('CRIT')
+		detect(ON_Condition_WARN, off=when(signal is None, '${var.unavailable_sending_operations_clear_duration}')).publish('WARN')
+  EOF
 
   rule {
     description           = "are too high > ${var.unavailable_sending_operations_threshold_critical}"
@@ -55,20 +58,21 @@ resource "signalfx_detector" "unavailable_sending_operations" {
     notifications         = coalescelist(var.unavailable_sending_operations_notifications_warning, var.unavailable_sending_operations_notifications, var.notifications)
     parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
   }
-
 }
 
 resource "signalfx_detector" "unavailable_sending_operations_ratio" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP Pub/Sub Topic sending unavailable messages ratio"
 
   program_text = <<-EOF
-		from signalfx.detectors.aperiodic import aperiodic
+		from signalfx.detectors.aperiodic import conditions
 		A = data('topic/send_message_operation_count', filter=filter('monitored_resource', 'pubsub_topic') and filter('response_code', 'unavailable') and ${module.filter-tags.filter_custom})${var.unavailable_sending_operations_ratio_aggregation_function}
 		B = data('topic/send_message_operation_count', filter=filter('monitored_resource', 'pubsub_topic') and ${module.filter-tags.filter_custom})${var.unavailable_sending_operations_ratio_aggregation_function}
 		signal = ((100*A)/B).${var.unavailable_sending_operations_ratio_transformation_function}(over='${var.unavailable_sending_operations_ratio_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.unavailable_sending_operations_ratio_threshold_critical}, 'above', lasting('${var.unavailable_sending_operations_ratio_aperiodic_duration}', ${var.unavailable_sending_operations_ratio_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.unavailable_sending_operations_ratio_threshold_warning}, ${var.unavailable_sending_operations_ratio_threshold_critical}, 'within_range', lasting('${var.unavailable_sending_operations_ratio_aperiodic_duration}', ${var.unavailable_sending_operations_ratio_aperiodic_percentage}), upper_strict=False).publish('WARN')
-	EOF
+		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.unavailable_sending_operations_ratio_threshold_critical}, ${var.unavailable_sending_operations_ratio_threshold_critical}, 'above', lasting('${var.unavailable_sending_operations_ratio_aperiodic_duration}', ${var.unavailable_sending_operations_ratio_aperiodic_percentage}), 'observed')
+		ON_Condition_WARN = conditions.generic_condition(signal, ${var.unavailable_sending_operations_ratio_threshold_warning}, ${var.unavailable_sending_operations_ratio_threshold_critical}, 'within_range', lasting('${var.unavailable_sending_operations_ratio_aperiodic_duration}', ${var.unavailable_sending_operations_ratio_aperiodic_percentage}), 'observed', strict_2=False)
+		detect(ON_Condition_CRIT, off=when(signal is None, '${var.unavailable_sending_operations_ratio_clear_duration}')).publish('CRIT')
+		detect(ON_Condition_WARN, off=when(signal is None, '${var.unavailable_sending_operations_ratio_clear_duration}')).publish('WARN')
+  EOF
 
   rule {
     description           = "is too high >= ${var.unavailable_sending_operations_ratio_threshold_critical}"
@@ -87,5 +91,4 @@ resource "signalfx_detector" "unavailable_sending_operations_ratio" {
     notifications         = coalescelist(var.unavailable_sending_operations_ratio_notifications_warning, var.unavailable_sending_operations_ratio_notifications, var.notifications)
     parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
   }
-
 }
